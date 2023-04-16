7cf0d67b1fadea96e6d650edfae5b54b50a5d728
sunnysamuelms <sunnysamuel2112@gmail.com>
2023-04-17 02:47:20 +0530

    added color changing event

--- myjs.js
+++ myjs.js
@@ -1,8 +1,9 @@
 // confirm("from JavaScript file")
-
+$("#h1").click(function () {
+    alert("You clicked a paragraph!");
+});
 var a = 10
 var b = 20
 var c = a + b
-document.write(c)
+document.write(c);
 
-console.log("hello");
